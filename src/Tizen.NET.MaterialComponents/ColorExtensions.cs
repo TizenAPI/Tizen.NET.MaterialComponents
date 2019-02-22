0dc773b7a8453111fe2f2d6a43f48a39620f9ff9
shyun.min <shyun.min@samsung.com>
2019-02-22 18:15:49 +0900

    Add MTextField

---
+++ src/Tizen.NET.MaterialComponents/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using ElmSharp;
+
+namespace Tizen.NET.MaterialComponents
+{
+    public static class ColorExtensions
+    {
+        internal static Color Blending(this Color color, Color blendingColor, int density = 80)
+        {
+            var r = color.R + (blendingColor.R - color.R) * density / 100;
+            var g = color.G + (blendingColor.G - color.G) * density / 100;
+            var b = color.B + (blendingColor.B - color.B) * density / 100;
+
+            return new Color(r, g, b);
+        }
+    }
+}
--- src/Tizen.NET.MaterialComponents/Constants.cs
+++ src/Tizen.NET.MaterialComponents/Constants.cs
@@ -3,17 +3,38 @@
     public static class Styles
     {
         public static readonly string Material = "material";
+        public static readonly string Singleline = "singleline";
     }
 
-    public static class PartName
+    public static class Parts
     {
         public static readonly string Content = "elm.swallow.content";
         public static readonly string Border = "border";
+        public static readonly string Guide = "elm.guide";
+        public static readonly string Label = "label";
+        public static readonly string TextLabel = "elm.text.label";
+        public static readonly string TextEdit = "text_edit";
+        public static readonly string TextEditFocused = "text_edit_focused";
+        public static readonly string Underline = "underline";
+        public static readonly string UnderlineFocused = "underline_focused";
     }
 
-    public static class Action
+    public static class Actions
     {
         public static readonly string ShowShadow = "elm,action,show,shadow";
         public static readonly string HideShadow = "elm,action,hide,shadow";
+    }
+
+    public static class States
+    {
+        public static readonly string Activate = "elm,state,activate";
+        public static readonly string Unactivate = "elm,state,unactivate";
+        public static readonly string Focused = "elm,state,focused";
+        public static readonly string Unfocused = "elm,state,unfocused";
+    }
+
+    public static class Events
+    {
+        public static readonly string Changed = "changed";
     }
 }
